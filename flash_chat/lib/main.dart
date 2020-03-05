b7f8e3bf0976468fea35197131054f6b114dd71d
Keheira <keheirah@gmail.com>
2020-03-05 01:09:40 -0500

    Not done

---
+++ flash_chat/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/chat_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          textTheme: TextTheme(
+            body1: TextStyle(color: Colors.black45),
+          ),
+        ),
+        initialRoute: 'welcome',
+        routes: {
+          'welcome': (context) => WelcomeScreen(),
+          'login': (context) => LoginScreen(),
+          'registration': (context) => RegistrationScreen(),
+          'chat': (context) => ChatScreen(),
+        });
+  }
+}
